fae888014cc54edaf0c6b0b6181c900309a1b786
Rick Bacci <rickbacci@gmail.com>
2015-09-23 18:48:30 -0600

    add classes to panels
    The timer now works when you drag a card to
    'in progress', any other column will not trigger
    the timer. The classes however are updated
    to reflect the column that they are in.

--- app/assets/javascripts/moveCards.js
+++ app/assets/javascripts/moveCards.js
@@ -5,9 +5,72 @@ $(document).ready(function() {
   });
 
   $( ".panel" ).draggable({
+    start: function(event, ui) {
+      console.log('dragging started');
+
+      // var className = $(ui.draggable).attr('class');
+    },
+    drag: function() {
+      console.log('dragging in progress');
+    },
+    stop: function(event, ui) {
+      console.log('dragging stopped');
+
+      var backlog    = this.parentElement.className.includes('backlog');
+      var ready      = this.parentElement.className.includes('ready');
+      var inProgress = this.parentElement.className.includes('in-progress');
+      var completed  = this.parentElement.className.includes('completed');
+
+      if (backlog) {
+        $(this).addClass('backlog');
+        $(this).removeClass('in-progress ready completed');
+      }
+
+
+      if (ready) {
+        $(this).addClass('ready');
+        $(this).removeClass('backlog in-progress completed');
+      }
+
+      if (inProgress) {
+        $(this).addClass('in-progress');
+        $(this).removeClass('backlog ready completed');
+
+        window.setTimeout(giveAlert, 2000);
+      }
+
+      if (completed) {
+        $(this).addClass('completed');
+        $(this).removeClass('backlog ready in-progress');
+      }
+
+    },
     connectToSortable: ".sortable",
-    // revert: "invalid",
+    revert: "invalid",
     cursor: "move"
   });
 
 });
+
+function giveAlert() {
+  alert("Times up!");
+}
+
+function createPost() {
+  $("#create-post").on("click", function(){
+    var postParams = {
+      post: {
+        description: $("#post-description").val()
+      }
+    }
+
+    $.ajax({
+      type:    "POST",
+      url:     "https://turing-birdie.herokuapp.com/api/v1/posts.json",
+      data:    postParams,
+      success: function(post) {
+        renderPost(post)
+      }
+    })
+  })
+}
