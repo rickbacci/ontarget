0175402b913175cda6d880d46edab2bdfec86763
Rick Bacci <rickbacci@gmail.com>
2015-09-26 20:35:18 -0600

    refactor projects controller and show view

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -7,12 +7,10 @@ class ProjectsController < ApplicationController
   def show
     @project = Project.find(params[:id])
 
-    client.repo = @project.name
+    current_user.current_project = @project.name
+    current_user.save
 
-    @backlog     = client.issues.list(user: 'rickbacci', repo: @project.name, labels: 'backlog')
-    @ready       = client.issues.list(user: 'rickbacci', repo: @project.name, labels: 'ready')
-    @in_progress = client.issues.list(user: 'rickbacci', repo: @project.name, labels: 'in-progress')
-    @completed   = client.issues.list(user: 'rickbacci', repo: @project.name, labels: 'completed')
+    @issues = client.issues.list(repo: @project.name)
   end
 
   def new
@@ -24,13 +22,8 @@ class ProjectsController < ApplicationController
   def create
     project = current_user.projects.create(name: params[:name])
 
-    client.repo = project.name
-    current_user.current_project = project.name
-
-    client.issues.labels.create name: 'backlog', color: '1FFFFF'
-    client.issues.labels.create name: 'ready', color: 'F3FFFF'
-    client.issues.labels.create name: 'in-progress', color: 'FF5FFF'
-    client.issues.labels.create name: 'completed', color: 'FFF7FF'
+    update_project_name(project)
+    create_labels
 
     if project
       flash[:success] = "Repository successfully added to your project list!"
@@ -63,4 +56,18 @@ class ProjectsController < ApplicationController
                   user:          current_user.nickname,
                   repo:          current_user.current_project)
   end
+
+  def create_labels
+    client.issues.labels.create name: 'backlog', color: '1FFFFF'
+    client.issues.labels.create name: 'ready', color: 'F3FFFF'
+    client.issues.labels.create name: 'in-progress', color: 'FF5FFF'
+    client.issues.labels.create name: 'completed', color: 'FFF7FF'
+  end
+
+  def update_project_name(project)
+    client.repo = project.name
+    current_user.current_project = project.name
+    current_user.save
+  end
+
 end
