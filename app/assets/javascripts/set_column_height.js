69a4b5579bb8e4ad16bb84b0eae9c373b1bb6278
Rick Bacci <rickbacci@gmail.com>
2016-04-05 11:56:20 -0400

    add logic to set_column_height
    - better define class names in view
    - fix jshint errors

--- app/assets/javascripts/set_column_height.js
+++ app/assets/javascripts/set_column_height.js
@@ -1,4 +1,16 @@
 function setColumnHeight() {
-  $('.column').height($('.col').height());
+
+  var largestHeight = 0;
+  var columns =  $('.status-list');
+
+  $('.status-list').height(0);
+
+  $.each( columns, function (i, column, c) {
+    if (column.scrollHeight >= largestHeight) {
+      largestHeight = column.scrollHeight;
+    }
+  });
+
+  $('.status-list').height(largestHeight);
 }
 
