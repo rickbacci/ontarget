020fa0ed1b02e1db9fd5dd51c9fbeb52fd9ad995
Rick Bacci <rickbacci@gmail.com>
2016-04-12 18:54:38 -0400

    add jQuery to filter repo names

--- app/assets/javascripts/watch_cards.js
+++ app/assets/javascripts/watch_cards.js
@@ -5,5 +5,17 @@ $(document).ready(function() {
   makeCardsSortable();
   makeCardsDraggable();
 
+  $('input#search_repos').on ('keyup', function() {
+    var value = $(this).val().toLowerCase();
+
+    $('.repo-btn').each(function(){
+      if ($(this).text().toLowerCase().includes(value)) {
+        $(this).parent().show();
+      } else {
+        $(this).parent().hide();
+      }
+    });
+
+  });
 });
 
