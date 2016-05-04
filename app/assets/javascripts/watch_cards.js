8125274c53d32be4adf3da5b489b49529d9239ef
Rick Bacci <rickbacci@gmail.com>
2016-05-03 20:46:09 -0400

    add labels for all cards with ajax

--- app/assets/javascripts/watch_cards.js
+++ app/assets/javascripts/watch_cards.js
@@ -15,5 +15,35 @@ $(document).ready(function() {
     $('.timer-time-' + num).val(time);
   });
 
+  $('.cards').each(function(i, card) {
+    var issueNumber = card.dataset.number;
+    $.ajax({
+      type: "GET",
+      url: '/issue_labels/' + issueNumber,
+      success: function(returnData) {
+        var issueNumber = this.dataset.number;
+        var labels;
+
+        labels =  $.map(returnData, function(label){
+          return "<div class='btn btn-xs card-labels "
+          + label.name + "' style='background-color:#"
+          + label.color + ";margin-top:10px'" + ">"
+          + label.name
+          + "</div>";
+        });
+
+        $('.issue-labels-' + issueNumber).html(labels)
+
+        hideDataLabels();
+
+      }.bind(this),
+      error: function(xhr) {
+        console.log(xhr.responseText);
+      }
+    });
+
+  });
+
+
 });
 
