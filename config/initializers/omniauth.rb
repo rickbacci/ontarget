d620617f82274294a58e186b6ea91490278fbfb1
rickbacci <rickbacci@gmail.com>
2020-02-04 21:02:04 -0700

    add comment

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,7 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
+  # TODO: do I need this?
+  # provider :developer unless Rails.env.production?
+
   provider :github,
   ENV['github_id'],
   ENV['github_secret'],
