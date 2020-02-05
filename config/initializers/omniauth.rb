f640a63c6f629bc13f469288936cd2f66bbd4a16
rickbacci <rickbacci@gmail.com>
2020-02-04 21:31:09 -0700

    remove un-needed scope

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -5,5 +5,5 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :github,
   ENV['github_id'],
   ENV['github_secret'],
-  scope: "user,repo, delete_repo"
+  scope: "read:user,public_repo"
 end
